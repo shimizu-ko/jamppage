551d6df583f38307bad920c7b8e7349575367ded
shimizu-ko <52282454+shimizu-ko@users.noreply.github.com>
2022-04-25 21:02:41 +0900

    Add files via upload

---
+++ rob.py
@@ -0,0 +1,14 @@
+
+from argparse import ArgumentParser
+import urllib.request
+
+def get_option():
+    argparser = ArgumentParser()
+    argparser.add_argument('-u', '--url', help='url')
+    return argparser.parse_args()
+
+if __name__ == '__main__':
+    args = get_option()
+    with open('rob.html' ,'w',encoding='utf-8') as file:
+        html=urllib.request.urlopen(args.url)
+        file.write(html.read().decode('utf-8'))
