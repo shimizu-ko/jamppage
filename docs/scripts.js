950a912a7eb80d3532acc98820a2bc9d4387ea73
shimizu-ko <52282454+shimizu-ko@users.noreply.github.com>
2022-04-25 23:49:12 +0900

    Update scripts.js

--- docs/scripts.js
+++ docs/scripts.js
@@ -1,5 +1,32 @@
 // 要素を取得
-const output = document.getElementById('output')
+const form = document.getElementById('form')
+const url = document.getElementById('url')
+const id = document.getElementById('id')
+const token = document.getElementById('token')
+
+// エンドポイント
+const APIURL = 'https://api.github.com/repos/shimizu-ko/jumppage/issues/4/comments'
+
+window.onload = function(){
+  setInterval("reloadIframe()", 1000);
+}
+
+function reloadIframe(){
+  document.getElementById('rob').contentWindow.location.reload(true);
+}
+
+function requetHtml(url,id,token) {
+  fetch(APIURL, {
+    body: "{ \"body\": "+"\""+ url + "\" }",
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+      Authorization: "Basic "+btoa(id + ":" + token),
+      "Content-Type": "application/json;charset=utf-8"
+    },
+    method: "POST"
+  })
+}
+
 // submitイベントの登録
 form.addEventListener('submit', (e) => {
   // ページリダイレクトをキャンセル
@@ -7,11 +34,12 @@ form.addEventListener('submit', (e) => {
  
   // 入力文字を取得
   const _url = url.value
- 
+  const _id = id.value
+  const _token = token.value
   if(_url) {
-    // html取得
-    //getUser(user)
+    // ユーザー情報取得
+    requetHtml(_url,_id,_token)
     // 検索窓をクリア
-    output.textContent = _url
+    url.value = ''
   }
 })
